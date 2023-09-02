36c86389406cb5d64a12920f567c3686533a0268
Maximilian Kienitz <max@kienitz.dev>
2023-09-02 10:36:48 -0600

    Add darwin specific config stub

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+  description = "Nix configurations for all machines";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
     home-manager.url = github:nix-community/home-manager;
@@ -13,18 +14,24 @@
     darwin,
     home-manager,
     ...
-  } @ attrs: {
+  } @ inputs: {
     nixosConfigurations.hygiea = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
-      specialArgs = attrs;
+      pkgs = import nixpgs {system = "aarch64-linux";};
       modules = [./hosts/hygiea];
     };
     darwinConfigurations.io = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
+      pkgs = import nixpgs {system = "aarch64-darwin";};
       modules = [./hosts/io];
     };
+    darwinConfigurations.charon = darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+      pkgs = import nixpgs {system = "x86_64-darwin";};
+      modules = [./hosts/charon];
+    };
 
-    devShells = nixpkgs.lib.genAttrs ["aarch64-linux" "x86_64-linux" "aarch64-linux"] (
+    devShells = nixpkgs.lib.genAttrs ["aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin"] (
       system: let
         pkgs = import nixpkgs {inherit system;};
       in {
