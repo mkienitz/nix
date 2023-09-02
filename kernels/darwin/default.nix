6c8cdab84d4fe41a05399d422de26b403a60f69f
Maximilian Kienitz <max@kienitz.dev>
2023-09-02 19:21:03 +0200

    Relocate initial darwin config

---
+++ kernels/darwin/default.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  ...
+}: {
+  programs.zsh.enable = true;
+  environment = {
+    shells = with pkgs; [bash zsh];
+    loginShell = pkgs.zsh;
+    systemPackages = with pkgs; [coreutils];
+    fonts = {
+      fontDir.enable = true;
+      fonts = with pkgs; [(nerdfonts.override {fonts = ["JetBrainsMono"];})];
+    };
+  };
+  services.nix-daemon.enable = true;
+  services.nix-daemon.package = pkgs.nixFlakes;
+  system.defaults = {
+    finder = {
+      AppleShowAllExtensions = true;
+      _FXShowPosixPathInTitle = true;
+    };
+    dock.autohide = true;
+    NSGlobalDomain = {
+      AppleShowAllExtensions = true;
+      InitialKeyRepeat = 14;
+      KeyRepeat = 1;
+    };
+  };
+}
