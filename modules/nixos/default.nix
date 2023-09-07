faed384a3ac5c9ee8893016934446b21b61d7769
Maximilian Kienitz <max@kienitz.dev>
2023-09-07 22:26:36 +0200

    feat: add yubikey ssh auth

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -31,16 +31,21 @@
 
   time.timeZone = "Europe/Berlin";
 
-  users = {
+  users = let
+    authorizedKeys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKbdbkaOUCITGKH/XfDVg00dPE+iRHPFhNIUZ/SK+rbmAAAAC3NzaDpHZW5lcmFs Yubikey_5C_NFC_General"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVcPOZFxCD1qSqCVXp5XYQ+yqJxI5kJ6PapuSOmnAIU MBP"
+    ];
+  in {
     mutableUsers = false;
     users.max = {
       shell = pkgs.zsh;
       isNormalUser = true;
       hashedPassword = "$y$j9T$VCnJOOcqEduAbjfUu3lg.1$c6nV8lybLzpG1MMFicsvuL/AwUUni.4Zd9aCIbyJsMB";
       extraGroups = ["wheel"];
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVcPOZFxCD1qSqCVXp5XYQ+yqJxI5kJ6PapuSOmnAIU MBP"];
+      openssh.authorizedKeys.keys = authorizedKeys;
     };
-    users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVcPOZFxCD1qSqCVXp5XYQ+yqJxI5kJ6PapuSOmnAIU MBP"];
+    users.root.openssh.authorizedKeys.keys = authorizedKeys;
   };
 
   system.stateVersion = "23.11";
