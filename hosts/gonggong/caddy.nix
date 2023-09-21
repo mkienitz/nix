7d215f391c88531bd5d19817862ba523dae0f3d1
Maximilian Kienitz <max@kienitz.dev>
2023-09-21 06:09:51 +0200

    refactor: slim down gonggong config

--- hosts/gonggong/caddy.nix
+++ hosts/gonggong/caddy.nix
@@ -1,40 +1,11 @@
-{pkgs, ...}: {
+{...}: {
+  networking.firewall.allowedTCPPorts = [80 443];
   services.caddy = {
     enable = true;
-    globalConfig = ''
-      auto_https off
-      admin off
-    '';
-    extraConfig = ''
-      (origin_pull) {
-      	tls {
-      		client_auth {
-      			mode require_and_verify
-      			trusted_ca_cert_file ${pkgs.fetchurl {
-        url = "https://developers.cloudflare.com/ssl/static/authenticated_origin_pull_ca.pem";
-        hash = "";
-      }}
-      		}
-      	}
-      }
-    '';
     virtualHosts = {
-      bipper = {
-        hostName = "bipper.maxkienitz.com";
-        extraConfig = ''
-          import origin_pull
-          handle /api/* {
-          	reverse_proxy bipper:8000
-          }
-          root * /static/bipper-frontend
-          file_server
-        '';
-      };
       default = {
-        hostName = "maxkienitz.com";
-        serverAliases = ["*.maxkienitz.com"];
+        hostName = "kienitz.dev";
         extraConfig = ''
-          import origin_pull
           header {
           	Content-Type text/html
           }
@@ -42,18 +13,9 @@
         '';
       };
       paypal = {
-        hostName = "paypal.maxkienitz.com";
+        hostName = "paypal.kienitz.dev";
         extraConfig = ''
-          import origin_pull
           redir https://www.paypal.com/paypalme/maximiliankienitz
-        '';
-      };
-      files = {
-        hostName = "files.maxkienitz.com";
-        extraConfig = ''
-          import origin_pull
-          root * /static/files
-          file_server browse
         '';
       };
     };
--- hosts/gonggong/default.nix
+++ hosts/gonggong/default.nix
@@ -1,57 +1,13 @@
 {
   config,
-  modulesPath,
   lib,
   ...
 }: {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
+    ./hw-specific.nix
     ../../modules/nixos
-    ../../users/max
     ./caddy.nix
   ];
 
   deployment.buildOnTarget = false;
-
-  # Hardware specific
-  boot = {
-    initrd = {
-      availableKernelModules = ["xhci_pci" "usbhid" "sr_mod"];
-      kernelModules = [];
-    };
-    kernelModules = [];
-    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-    extraModulePackages = [];
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-  };
-
-  fileSystems."/" = {
-    device = "rpool/root";
-    fsType = "zfs";
-    options = ["zfsutil"];
-  };
-
-  fileSystems."/home/max" = {
-    device = "rpool/home";
-    fsType = "zfs";
-    options = ["zfsutil"];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/4A0D-5796";
-    fsType = "vfat";
-  };
-
-  swapDevices = [];
-
-  networking = {
-    useDHCP = lib.mkDefault true;
-    hostId = "600a1d45";
-    hostName = "gonggong";
-  };
-
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
