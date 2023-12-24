247ae77ae8e55c9fc7fd5b7d25aa0f6da5009bb7
Maximilian Kienitz <max@kienitz.dev>
2023-12-24 06:11:49 +0100

    refactor(hygiea): bring config structure in line with other machines

--- hosts/hygiea/default.nix
+++ hosts/hygiea/default.nix
@@ -1,29 +1,7 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ../../modules/nixos
+    ./hw-specific.nix
+    ./services
   ];
-
-  # Hardware specific
-  boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-    };
-  };
-
-  hardware.enableRedistributableFirmware = true;
-
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = ["noatime"];
-    };
-  };
-
-  networking = {
-    hostName = "hygiea";
-  };
 }
---
+++ hosts/hygiea/hw-specific.nix
@@ -0,0 +1,28 @@
+{pkgs, ...}: {
+  imports = [
+    ../../modules/nixos
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  hardware.enableRedistributableFirmware = true;
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+  };
+
+  networking = {
+    hostName = "hygiea";
+  };
+}
