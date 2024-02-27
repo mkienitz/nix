7b7c3d796cb9eb676ffbc6016c376d66c61d4ffc
Maximilian Kienitz <max@kienitz.dev>
2024-02-27 16:49:33 +0100

    refactor: move acme setup into separate module

--- hosts/hygiea/services/nginx.nix
+++ hosts/hygiea/services/nginx.nix
@@ -1,49 +1,26 @@
-{config, ...}: {
-  age.secrets.acme-cloudflare-zone-token.rekeyFile = ../secrets/acme-cloudflare-zone-token.age;
-  age.secrets.acme-cloudflare-dns-token.rekeyFile = ../secrets/acme-cloudflare-dns-token.age;
+{config, ...}: let
+  coffee-labeler-domain = "label.maxkienitz.com";
+in {
+  imports = [../../../modules/nixos/acme/maxkienitz.com];
 
   networking.firewall.allowedTCPPorts = [80 443];
-  security.acme = {
-    acceptTerms = true;
-    defaults = {
-      email = "contact@maxkienitz.com";
-      dnsProvider = "cloudflare";
-      dnsResolver = "1.1.1.1:53";
-      dnsPropagationCheck = true;
-      reloadServices = ["nginx"];
-      credentialFiles = {
-        CF_ZONE_API_TOKEN_FILE = config.age.secrets.acme-cloudflare-zone-token.path;
-        CF_DNS_API_TOKEN_FILE = config.age.secrets.acme-cloudflare-dns-token.path;
-      };
-      group = "nginx";
-    };
-    certs = {
-      "label.maxkienitz.com".inheritDefaults = true;
-    };
-  };
+  security.acme.certs.${coffee-labeler-domain}.inheritDefaults = true;
 
   services.nginx = {
     enable = true;
-    virtualHosts = let
-      defaults = {
-        forceSSL = true;
-        useACMEHost = "label.maxkienitz.com";
+    virtualHosts.${coffee-labeler-domain} = {
+      forceSSL = true;
+      useACMEHost = coffee-labeler-domain;
+      locations."/" = {
+        proxyPass = let
+          inherit (config.services.coffee-labeler-web) address port;
+        in "http://${address}:${toString port}/";
+        extraConfig = ''
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
       };
-    in {
-      "label.maxkienitz.com" =
-        defaults
-        // {
-          locations."/" = {
-            proxyPass = let
-              inherit (config.services.coffee-labeler-web) address port;
-            in "http://${address}:${toString port}/";
-            extraConfig = ''
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-Host $host;
-              proxy_set_header X-Forwarded-Proto $scheme;
-            '';
-          };
-        };
     };
   };
 }
---
+++ modules/nixos/acme/maxkienitz.com/default.nix
@@ -0,0 +1,19 @@
+{config, ...}: {
+  age.secrets.acme-cloudflare-zone-token.rekeyFile = ./secrets/acme-cloudflare-zone-token.age;
+  age.secrets.acme-cloudflare-dns-token.rekeyFile = ./secrets/acme-cloudflare-dns-token.age;
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "contact@maxkienitz.com";
+      dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1:53";
+      dnsPropagationCheck = true;
+      reloadServices = ["nginx"];
+      credentialFiles = {
+        CF_ZONE_API_TOKEN_FILE = config.age.secrets.acme-cloudflare-zone-token.path;
+        CF_DNS_API_TOKEN_FILE = config.age.secrets.acme-cloudflare-dns-token.path;
+      };
+      group = "nginx";
+    };
+  };
+}
