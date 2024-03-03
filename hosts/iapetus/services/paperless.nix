b05285c5554fc130b471a85cb745cb95f79b3dc1
Maximilian Kienitz <max@kienitz.dev>
2024-03-03 07:16:04 +0100

    feat(paperless): switch from SMB to FTPS

--- hosts/iapetus/services/paperless.nix
+++ hosts/iapetus/services/paperless.nix
@@ -1,92 +1,139 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: let
   paperlessDomain = "paperless.maxkienitz.com";
+  paperlessConsumePath = "/paperless/consume";
+  ftps_psv_min_port = 56260;
+  ftps_psv_max_port = ftps_psv_min_port;
 in {
+  # Setup ACME
   imports = [../../../modules/nixos/acme/maxkienitz.com];
-
-  age.secrets.smb-paperless-credentials.rekeyFile = ../secrets/smb-paperless-credentials.age;
-  age.secrets.paperless-ngx-password = {
-    rekeyFile = ../secrets/paperless-ngx-password.age;
-    group = "paperless";
-    mode = "440";
-  };
-
-  environment.systemPackages = [pkgs.cifs-utils];
-  fileSystems."/paperless/consume" = {
-    device = "//mimas/paperless";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
-    in ["${automount_opts},vers=3.1.1,credentials=${config.age.secrets.smb-paperless-credentials.path},uid=paperless,gid=paperless"];
-  };
-
-  networking.firewall.allowedTCPPorts = [80 443];
-
   security.acme.certs.${paperlessDomain}.inheritDefaults = true;
 
-  # TODO: use upstreams
-  services.nginx = {
-    enable = true;
-    virtualHosts.${paperlessDomain} = {
-      forceSSL = true;
-      useACMEHost = paperlessDomain;
-      extraConfig = ''
-        client_max_body_size 512M;
-      '';
-      locations."/" = let
-        inherit (config.services.paperless) address port;
-      in {
-        proxyPass = "http://${address}:${builtins.toString port}";
-        proxyWebsockets = true;
-      };
+  age.secrets = {
+    # vsftpd requires the .db suffix for the user database
+    vsftpd-user-db = {
+      rekeyFile = ../secrets/vsftpd-user-db.age;
+      name = "vsftpUserDB.db";
+    };
+    paperless-ngx-password = {
+      rekeyFile = ../secrets/paperless-ngx-password.age;
+      group = "paperless";
+      mode = "440";
     };
   };
 
-  systemd.services.paperless-web.script = lib.mkBefore "mkdir -p /tmp/paperless";
+  networking.firewall = {
+    allowedTCPPorts = [21 80 443];
+    # Open port range for FTPS passive mode
+    allowedTCPPortRanges = [
+      {
+        from = ftps_psv_min_port;
+        to = ftps_psv_max_port;
+      }
+    ];
+  };
 
-  services.paperless = {
-    enable = true;
-    port = 10000;
-    address = "0.0.0.0";
-    passwordFile = config.age.secrets.paperless-ngx-password.path;
-    mediaDir = "/paperless/media";
-    consumptionDir = "/paperless/consume";
-    settings = {
-      PAPERLESS_URL = "https://${paperlessDomain}";
-      # PAPERLESS_ALLOWED_HOSTS = paperlessDomain;
-      PAPERLESS_CORS_ALLOWED_HOSTS = "https://${paperlessDomain}";
-      # PAPERLESS_TRUSTED_PROXIES = sentinelCfg.meta.wireguard.proxy-sentinel.ipv4;
+  systemd = {
+    # Create folder for scanner and share it with the paperless group
+    tmpfiles.rules = [
+      # "d /var/lib/vsftpd/es580w 2770 vsftpd paperless - -"
+      "d ${paperlessConsumePath} 2770 vsftpd paperless - -"
+    ];
+    # paperless-ngx doesn't setup its own SCRATCH_DIR for whatever reason
+    services.paperless-web.script = lib.mkBefore "mkdir -p /tmp/paperless";
+  };
 
-      # Ghostscript is entirely bug-free.
-      PAPERLESS_OCR_USER_ARGS = builtins.toJSON {
-        continue_on_soft_render_error = true;
+  services = {
+    # TODO: use upstreams
+    nginx = {
+      enable = true;
+      virtualHosts.${paperlessDomain} = {
+        forceSSL = true;
+        useACMEHost = paperlessDomain;
+        extraConfig = ''
+          client_max_body_size 512M;
+        '';
+        locations."/" = let
+          inherit (config.services.paperless) address port;
+        in {
+          proxyPass = "http://${address}:${builtins.toString port}";
+          proxyWebsockets = true;
+        };
       };
+    };
 
-      # virtiofsd doesn't send inotify events (not sure if generally, or because we
-      # mount the same host share on another vm (samba) and modify it there).
-      PAPERLESS_CONSUMER_POLLING = 1; # seconds
-      # Wait three seconds between file-modified checks. After 5 consecutive checks
-      # where the file wasn't modified it will be consumed.
-      PAPERLESS_CONSUMER_POLLING_DELAY = 3;
+    vsftpd = {
+      enable = true;
+      # Virtual users
+      enableVirtualUsers = true;
+      localUsers = true;
+      # TODO: report bug:
+      # nix module asserts that virtualUsers implies localUsers != null but that has a default value of false (always != null)
+      userDbPath = lib.removeSuffix ".db" config.age.secrets.vsftpd-user-db.path;
+      # User needs write permissions inside their chroot() jail
+      allowWriteableChroot = true;
+      chrootlocalUser = true;
+      localRoot = paperlessConsumePath;
+      virtualUseLocalPrivs = true;
+      writeEnable = true;
+      # SSL
+      forceLocalLoginsSSL = true;
+      forceLocalDataSSL = true;
+      rsaKeyFile = "/var/lib/acme/${paperlessDomain}/key.pem";
+      rsaCertFile = "/var/lib/acme/${paperlessDomain}/cert.pem";
+      extraConfig = ''
+        # Certain clients don't like the default
+        ssl_ciphers=HIGH
+        # Allow passive mode for some clients
+        pasv_enable=YES
+        pasv_min_port=${builtins.toString ftps_psv_min_port}
+        pasv_max_port=${builtins.toString ftps_psv_max_port}
+        # Delete only 'others' permissions
+        local_umask=0007
+        # Both 'vsftpd' owner and 'paperless' group need r/w
+        chown_upload_mode=0660
+        # Also log FTP transfers
+        log_ftp_protocol=YES
+        xferlog_enable=YES
+      '';
+    };
 
-      PAPERLESS_CONSUMER_ENABLE_BARCODES = true;
-      PAPERLESS_CONSUMER_ENABLE_ASN_BARCODE = true;
-      PAPERLESS_CONSUMER_BARCODE_SCANNER = "ZXING";
-      PAPERLESS_CONSUMER_RECURSIVE = true;
-      # PAPERLESS_FILENAME_FORMAT = "{owner_username}/{created_year}-{created_month}-{created_day}_{asn}_{title}";
+    paperless = {
+      enable = true;
+      port = 10000;
+      address = "0.0.0.0";
+      passwordFile = config.age.secrets.paperless-ngx-password.path;
+      mediaDir = "/paperless/media";
+      consumptionDir = paperlessConsumePath;
+      settings = {
+        PAPERLESS_URL = "https://${paperlessDomain}";
+        # TODO: set before doing fresh install
+        # PAPERLESS_ADMIN_USER = "";
 
-      # Nginx does that better.
-      PAPERLESS_ENABLE_COMPRESSION = false;
+        # Ghostscript is entirely bug-free.
+        PAPERLESS_OCR_USER_ARGS = builtins.toJSON {
+          continue_on_soft_render_error = true;
+        };
 
-      #PAPERLESS_IGNORE_DATES = concatStringsSep "," ignoreDates;
-      PAPERLESS_NUMBER_OF_SUGGESTED_DATES = 8;
-      PAPERLESS_OCR_LANGUAGE = "deu+eng";
-      PAPERLESS_TASK_WORKERS = 4;
-      PAPERLESS_WEBSERVER_WORKERS = 4;
+        PAPERLESS_CONSUMER_ENABLE_BARCODES = true;
+        PAPERLESS_CONSUMER_ENABLE_ASN_BARCODE = true;
+        PAPERLESS_CONSUMER_BARCODE_SCANNER = "ZXING";
+        PAPERLESS_CONSUMER_RECURSIVE = true;
+        PAPERLESS_FILENAME_FORMAT = "{created_year}-{created_month}-{created_day}_{asn}_{title}";
+        # PAPERLESS_FILENAME_FORMAT_REMOVE_NONE = true;
+
+        # Let nginx handle compression
+        PAPERLESS_ENABLE_COMPRESSION = false;
+
+        #PAPERLESS_IGNORE_DATES = concatStringsSep "," ignoreDates;
+        PAPERLESS_NUMBER_OF_SUGGESTED_DATES = 8;
+        PAPERLESS_OCR_LANGUAGE = "deu+eng";
+        PAPERLESS_TASK_WORKERS = 4;
+        PAPERLESS_WEBSERVER_WORKERS = 4;
+      };
     };
   };
 }
