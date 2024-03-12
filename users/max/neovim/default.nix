f472007294758575fa04aab91043651791b8d55b
Maximilian Kienitz <max@kienitz.dev>
2024-03-12 08:44:21 +0100

    feat(neovim): add general keymaps

--- users/max/neovim/default.nix
+++ users/max/neovim/default.nix
@@ -1,14 +1,14 @@
 {
   imports = [
-    ./plugins
-    ./filetypes.nix
-    ./options.nix
     ./colorscheme.nix
+    ./filetypes.nix
+    ./keymap.nix
+    ./options.nix
+    ./plugins
   ];
   programs.nixvim = {
     enable = true;
     vimAlias = true;
     viAlias = true;
-    globals.mapleader = " ";
   };
 }
