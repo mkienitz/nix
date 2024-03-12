7322c869535934ad07e17a20e42f348f85556296
Maximilian Kienitz <max@kienitz.dev>
2024-03-12 03:07:16 +0100

    feat(neovim): enable lua highlighting inside nix

--- users/max/neovim/plugins/nvim-window-picker.nix
+++ users/max/neovim/plugins/nvim-window-picker.nix
@@ -3,21 +3,25 @@
     extraPlugins = with pkgs.vimPlugins; [
       nvim-window-picker
     ];
-    extraConfigLuaPost = ''
-      require("window-picker").setup {
-        hint = "floating-big-letter",
-        filter_rules = {
-          bo = {
-            filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
-            buftype = { "terminal", "quickfix", "prompt" },
+    extraConfigLuaPost =
+      /*
+      lua
+      */
+      ''
+        require("window-picker").setup {
+          hint = "floating-big-letter",
+          filter_rules = {
+            bo = {
+              filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
+              buftype = { "terminal", "quickfix", "prompt" },
+            },
           },
-        },
-        floating_big_letter = {
-          font = "ansi-shadow",
-        },
-        show_prompt = false,
-      }
-    '';
+          floating_big_letter = {
+            font = "ansi-shadow",
+          },
+          show_prompt = false,
+        }
+      '';
     keymaps = [
       {
         key = "<C-w><space>";
