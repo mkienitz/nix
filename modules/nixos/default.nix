093896cb3ea8458dbcf5e1de5f52e21ddd18a1b5
Maximilian Kienitz <max@kienitz.dev>
2024-04-05 00:52:20 +0200

    fix(ssh): properly declare host keys

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -19,6 +19,12 @@
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "yes";
     };
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
 
   security.sudo.enable = false;
