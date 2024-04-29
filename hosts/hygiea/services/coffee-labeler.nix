d1b910ed9796567dd163f984a3bbdcd80c31b81a
Maximilian Kienitz <max@kienitz.dev>
2024-04-29 18:21:28 +0200

    chore(hygiea): adjust label printer IP address

--- hosts/hygiea/services/coffee-labeler.nix
+++ hosts/hygiea/services/coffee-labeler.nix
@@ -5,7 +5,7 @@
     enable = true;
     address = "127.0.0.1";
     port = 10000;
-    printer-address = "192.168.178.36";
+    printer-address = "192.168.178.39";
     printer-port = 9100;
   };
 }
