687200b76e31f774a556332389342db7f349a3ea
Maximilian Kienitz <max@kienitz.dev>
2024-05-02 22:47:53 +0200

    feat(iapetus): add timer config to restic backup job

--- hosts/iapetus/services/restic.nix
+++ hosts/iapetus/services/restic.nix
@@ -15,6 +15,16 @@
       ];
       initialize = true;
       passwordFile = config.age.secrets.backup-password.path;
+      timerConfig = {
+        Persistent = true;
+        OnCalendar = "02:00";
+      };
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 12"
+        "--keep-yearly 75"
+      ];
     };
     openssh.knownHosts.sbox2 = {
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICf9svRenC/PLKIL9nk6K/pxQgoiFC41wTNvoIncOxs";
