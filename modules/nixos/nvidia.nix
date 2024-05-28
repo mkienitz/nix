ba0d979f7473619cfa1ab01c36c89676cc1f7ac2
Maximilian Kienitz <max@kienitz.dev>
2024-05-28 21:32:26 +0200

    feat(nixos): add nvidia config

---
+++ modules/nixos/nvidia.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-persistenced"
+      "nvidia-x11"
+      "nvidia-settings"
+    ];
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidiaPersistenced = true;
+  };
+}
