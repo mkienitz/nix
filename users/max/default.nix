12885afaeedb43678ebda26901cebc295b1fe762
Maximilian Kienitz <max@kienitz.dev>
2024-05-30 07:41:35 +0200

    feat(users/max): add doas and split into common and system-specific parts

--- users/max/default.nix
+++ users/max/default.nix
@@ -4,29 +4,43 @@
   lib,
   config,
   ...
-}: {
-  # Don't setup user on darwin
-  users.users.max = lib.mkIf (!pkgs.stdenv.isDarwin) {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    inherit (config.users.users.root) hashedPassword;
-  };
-  home-manager = {
-    useGlobalPkgs = true;
-    extraSpecialArgs = {
-      inherit inputs;
-    };
-    users.max = {
-      imports = [
-        ../../modules/hm/gui
-        ../../modules/hm/shell
-        ../../modules/hm/shell/neovim
-        # Always add impermanence options, even if they aren't used.
-        # This avoids the need for guards.
-        ../../modules/hm/impermanence.nix
+}:
+lib.mkMerge [
+  (
+    lib.mkIf pkgs.stdenv.isLinux
+    {
+      users.users.max = {
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        inherit (config.users.users.root) hashedPassword;
+      };
+      security.doas.enable = true;
+      security.doas.extraRules = [
+        {
+          users = ["max"];
+          keepEnv = true;
+        }
       ];
-      home.username = "max";
-      home.stateVersion = "23.11";
+    }
+  )
+  {
+    home-manager = {
+      useGlobalPkgs = true;
+      extraSpecialArgs = {
+        inherit inputs;
+      };
+      users.max = {
+        imports = [
+          ../../modules/hm/gui
+          ../../modules/hm/shell
+          ../../modules/hm/shell/neovim
+          # Always add impermanence options, even if they aren't used.
+          # This avoids the need for guards.
+          ../../modules/hm/impermanence.nix
+        ];
+        home.username = "max";
+        home.stateVersion = "23.11";
+      };
     };
-  };
-}
+  }
+]
