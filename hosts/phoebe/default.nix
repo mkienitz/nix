ba821fe37a5275aaf6e2d865936831cb0300203f
Maximilian Kienitz <max@kienitz.dev>
2024-06-08 17:36:07 +0200

    chore(phoebe): add agenix sandbox path

--- hosts/phoebe/default.nix
+++ hosts/phoebe/default.nix
@@ -29,6 +29,8 @@
   # FIXME: why does this not work if put into modules/nixos/default.nix?
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.extra-sandbox-paths = ["/tmp/agenix-rekey"];
+
   age.identityPaths = ["/persist/etc/ssh/ssh_host_ed25519_key"];
   age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBivT5T9lDMrIL+hhRNEPr03lsBsgBV5jsELi61FGcIo";
 }
