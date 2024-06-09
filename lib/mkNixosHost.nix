89608e949533af20c4f734f1a438d15f43fabec7
Maximilian Kienitz <max@kienitz.dev>
2024-06-09 17:50:26 +0200

    refactor: extract base nix module from mkNixosHost

--- lib/mkNixosHost.nix
+++ lib/mkNixosHost.nix
@@ -6,38 +6,13 @@ inputs.nixpkgs.lib.nixosSystem {
   };
   modules = [
     ../hosts/${hostName}
-    ({
-      lib,
-      config,
-      ...
-    }: {
-      config = {
-        nixpkgs = {
-          hostPlatform = arch;
-          inherit (inputs.self.pkgs.${arch}) overlays config;
-        };
-        networking.hostName = config.node.hostName;
+    ../modules/nixos
+    {
+      node.hostName = hostName;
+      nixpkgs = {
+        hostPlatform = arch;
+        inherit (inputs.self.pkgs.${arch}) overlays config;
       };
-      options = {
-        global.flakeRoot = lib.mkOption {
-          type = lib.types.path;
-          default = ../.;
-        };
-        node = {
-          baseDir = lib.mkOption {
-            type = lib.types.path;
-            default = ../hosts + "/${config.node.hostName}";
-          };
-          secretsDir = lib.mkOption {
-            type = lib.types.path;
-            default = config.node.baseDir + "/secrets";
-          };
-          hostName = lib.mkOption {
-            type = lib.types.str;
-            default = hostName;
-          };
-        };
-      };
-    })
+    }
   ];
 }
---
+++ modules/nixos/base.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    global.flakeRoot = lib.mkOption {
+      type = lib.types.path;
+      default = ../..;
+    };
+    node = {
+      baseDir = lib.mkOption {
+        type = lib.types.path;
+        default = ../../hosts + "/${config.node.hostName}";
+      };
+      secretsDir = lib.mkOption {
+        type = lib.types.path;
+        default = config.node.baseDir + "/secrets";
+      };
+      hostName = lib.mkOption {
+        type = lib.types.str;
+      };
+    };
+  };
+  config = {
+    networking.hostName = config.node.hostName;
+  };
+}
