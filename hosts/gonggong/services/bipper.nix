9714978a509167ef09ee2f96ad94e58a040605e1
Maximilian Kienitz <max@kienitz.dev>
2025-01-07 06:31:17 +0100

    chore(gonggong/bipper): adjust to new sqlite-based version

--- hosts/gonggong/services/bipper.nix
+++ hosts/gonggong/services/bipper.nix
@@ -1,32 +1,9 @@
 {
   inputs,
-  pkgs,
   ...
 }:
 {
   imports = [ inputs.bipper.nixosModules.default ];
-
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "bipper" ];
-    identMap = ''
-      # ArbitraryMapName systemUser DBUser
-      superuser_map      root      postgres
-      superuser_map      postgres  postgres
-      # Let other names login as themselves
-      superuser_map      /^(.*)$   \1
-    '';
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method optional_ident_map
-      local sameuser  all     peer        map=superuser_map
-    '';
-    ensureUsers = [
-      {
-        name = "bipper";
-        ensureDBOwnership = true;
-      }
-    ];
-  };
 
   services.bipper = {
     enable = true;
