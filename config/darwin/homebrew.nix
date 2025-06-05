dd814b1bda64004aecb20f4a94410c0ff10b7420
Maximilian Kienitz <max@kienitz.dev>
2025-06-05 03:27:18 +0200

    chore(darwin/homebrew): various cask changes

--- config/darwin/homebrew.nix
+++ config/darwin/homebrew.nix
@@ -13,35 +13,26 @@
       "m1ddc"
       "pinentry-mac"
     ];
-    taps = [
-      "nikitabobko/tap"
-    ];
     casks = [
       "adobe-creative-cloud"
-      "aerospace"
-      "balenaetcher"
       "bruno"
       "chatgpt"
       "discord"
       "docker"
       "google-chrome"
-      "jetbrains-toolbox"
       "karabiner-elements"
       "keyboardcleantool"
       "kitty"
       "macfuse"
       "monodraw"
       "mullvadvpn"
-      "obs"
       "obsidian"
       "orcaslicer"
-      "postman"
-      "raycast"
+      "rectangle"
       "signal"
       "spotify"
       "teamspeak-client"
-      "zed"
-      "zoom"
+      "zotero"
     ];
   };
 }
