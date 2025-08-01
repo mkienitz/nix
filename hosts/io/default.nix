ea68f8ef2411628ecd9d960e2b72f8fa8b6f7dc2
Maximilian Kienitz <max@kienitz.dev>
2025-08-01 20:00:14 +0200

    fix(io): import stylix in home-manager context

--- hosts/io/default.nix
+++ hosts/io/default.nix
@@ -2,7 +2,9 @@
   imports = [
     ../../config/darwin
     ../../config/darwin/homebrew.nix
-    ../../config/hm/gui/stylix.nix
     ../../config/darwin/hm.nix
+  ];
+  home-manager.users.max.imports = [
+    ../../config/hm/gui/stylix.nix
   ];
 }
