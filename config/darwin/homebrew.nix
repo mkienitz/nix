d835fb0ef4014e3c0acee75e53be2ef1c6f98804
Maximilian Kienitz <max@kienitz.dev>
2026-01-27 23:00:18 +0100

    feat(homebrew): update brews and casks

--- config/darwin/homebrew.nix
+++ config/darwin/homebrew.nix
@@ -11,27 +11,34 @@
     };
     brews = [
       "m1ddc"
+      "mole"
       "pinentry-mac"
     ];
     casks = [
       "adobe-creative-cloud"
       "bruno"
+      "calibre"
       "chatgpt"
       "discord"
       "docker-desktop"
       "google-chrome"
+      "jetbrains-toolbox"
       "karabiner-elements"
       "keyboardcleantool"
       "kitty"
+      "kicad"
+      "logi-options+"
       "macfuse"
       "monodraw"
-      "mullvadvpn"
+      "mullvad-vpn"
       "obsidian"
       "orcaslicer"
       "rectangle"
       "signal"
       "spotify"
       "teamspeak-client"
+      "zap"
+      "zoom"
       "zotero"
     ];
   };
