6014e99c186016ce0663e83f140f093b8aa29d04
Maximilian Kienitz <max@kienitz.dev>
2026-02-01 23:05:48 +0100

    feat(packages/cleanup): general improvements

--- packages/cleanup/cleanup.sh
+++ packages/cleanup/cleanup.sh
@@ -12,7 +12,7 @@ gum style \
   --padding "1 4" \
   "Direnv Cleanup Helper"
 
-find_direnvs () {
+find_direnvs() {
   sleep 1
   fd -t d -HI '.direnv' "$HOME"/git
 }
@@ -20,39 +20,34 @@ export -f find_direnvs
 
 found=$(gum spin --show-stdout --title "Looking for .direnv directories..." -- bash -c find_direnvs)
 
-
 if [[ -z "$found" ]]; then
   gum style "No .direnv directories found"
-  exit 0
+else
+  selected=$(echo "$found" | gum choose --padding "1 2" --no-limit --header "Found the following:")
+  if [[ -z "$selected" ]]; then
+    gum style --foreground 212 "No .direnv directories selected"
+  else
+    no_selected=$(echo "$selected" | wc -l)
+    # shellcheck disable=SC2001
+    gum format -- $'# Selected for deletion:\n'"$(echo "$selected" | sed 's/^/- /')"
+    gum confirm --selected.background 1 "Delete $no_selected directories?" ||
+    exit 0
+    echo "$selected" | xargs -I {} -- rm -rf "{}" &&
+    gum style --foreground 212 "✓ Deleted $no_selected directories"
+  fi
 fi
 
-selected=$(echo "$found" | gum choose --padding "1 2" --no-limit --header "Found the following:")
-if [[ -z "$selected" ]]; then
-  gum style "No .direnv directories selected"
-  exit 0
-fi
-no_selected=$(echo "$selected" | wc -l)
+gum confirm "Run garbage collection now? [requires sudo]" &&
+sudo -v &&
+gum spin --show-stdout --title $'Collecting garbage (sudo)...' -- sudo nix-collect-garbage -d --quiet 2>/dev/null |
+tail -n 1 | gum style --foreground 157 &&
+gum spin --show-stdout --title "Collecting garbage (user)..." -- nix-collect-garbage -d --quiet 2>/dev/null |
+tail -n 1 | gum style --foreground 157 &&
+gum style --foreground 212 "✓ Garbage collected"
 
+gum confirm "Run store optimisation now? [requires sudo]" &&
+sudo -v &&
+gum spin --show-stdout --title "Optimising store..." -- sudo nix store optimise 2>/dev/null &&
+gum style --foreground 212 "✓ Store optimised"
 
-# shellcheck disable=SC2001
-gum format -- $'# Selected for deletion:\n'"$(echo "$selected" | sed 's/^/- /')"
-
-gum confirm --selected.background 1  "Delete $no_selected directories?" \
-  && echo "$selected" | xargs -I {} -- rm -rf "{}" \
-  && gum style --foreground 212 "✓ Deleted $no_selected directories"
-
-gum confirm "Run garbage collection now? [requires sudo]" \
-  && sudo -v \
-  && gum spin --show-stdout --title $'Collecting garbage (sudo)...' -- sudo nix-collect-garbage -d --quiet \
-  | gum style --foreground 157 \
-  && gum spin --show-stdout --title "Collecting garbage (user)..." -- nix-collect-garbage -d --quiet \
-  | gum style --foreground 157 \
-  && gum style --foreground 212 --padding "1 2" "✓ Garbage collected"
-
-gum confirm "Run store optimisation now? [requires sudo]" \
-  && sudo -v \
-  && gum spin --show-stdout --title "Optimising store (sudo)..." -- sudo nix store optimise \
-  && gum spin --show-stdout --title "Optimising store (user)..." -- nix store optimise \
-  && gum style --foreground 212 --padding "1 2" "✓ Store optimised"
-
-gum style --foreground 212 --padding "1 2" "✓ Cleanup finished"
+gum style --foreground 212 "✓ Cleanup finished"
